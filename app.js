643a31b77b3567a4aa831a1702e8d73116c57fdc
smajaas <smajaas@gmail.com>
2023-10-14 14:58:15 +0530

    removed extended

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('./database/config/connect');
 
 // Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -13,7 +14,6 @@ const orderRoutes = require('./routes/orderRoutes');
 // Use the routes
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
-app.use(express.urlencoded({ extended: true }));
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
